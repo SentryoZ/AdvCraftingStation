29eedd8adbae8a71ae0bb633407998b13758b2ba
datnt <datnt@aipro-vn.com>
2025-03-05 11:14:58 +0700

    init

---
+++ src/main/java/me/sentryoz/advCraftingStation/AdvCraftingStation.java
@@ -0,0 +1,17 @@
+package me.sentryoz.advCraftingStation;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvCraftingStation extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
