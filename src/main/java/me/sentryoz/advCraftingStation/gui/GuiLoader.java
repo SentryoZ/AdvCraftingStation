cadbf5e70fd3bd560ae4ed38edb1b7dc3e3b4648
datnt <datnt@aipro-vn.com>
2025-03-05 17:02:49 +0700

    default thing

--- src/main/java/me/sentryoz/advCraftingStation/gui/GuiLoader.java
+++ src/main/java/me/sentryoz/advCraftingStation/gui/GuiLoader.java
@@ -1,8 +1,11 @@
 package me.sentryoz.advCraftingStation.gui;
 
-import me.sentryoz.advCraftingStation.AdvCraftingStation;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.Inventory;
 
 import java.io.File;
 import java.util.HashMap;
@@ -28,6 +31,9 @@ public class GuiLoader {
                 String stationName = fileName.substring(0, fileName.length() - 4); // Remove ".yml"
 
                 FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+                Inventory inventory = loadInventory(config);
+
                 stationConfigs.put(stationName, config);
 
                 plugin.getLogger().info("Loaded station config: " + stationName);
@@ -35,5 +41,16 @@ public class GuiLoader {
         } else {
             plugin.getLogger().warning("No station configuration files found.");
         }
+    }
+
+    public Inventory loadInventory(FileConfiguration config) {
+        String title = config.getString("title", "Crafting Station");
+        Integer size = config.getInt("rows", 1) * 9;
+        Component textComponent = Component.text(title);
+        Inventory inventory = Bukkit.createInventory(null, size, textComponent);
+
+        // build item stack
+
+        return inventory;
     }
 }
